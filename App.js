a3d9badf9facb343b8014e95b15d43f92a9ef45d
OtoSh8 <2201105@student.toa.edu.my>
2024-09-02 01:56:11 +0800

    Navigation Bar Integration
    Added Navigation Bar with press state

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import RootNavigator from "./screens/RootNavigator";
 import "./gesture-handler";
+import "./app.css";
 
 export default function App() {
   return <RootNavigator />;
--- screens/MainScreens.js
+++ screens/MainScreens.js
@@ -10,6 +10,7 @@ import DiscoverScreen from "./DiscoverScreen";
 import AppDrawer from "./AppDrawer";
 import SettingsScreen from "./SettingsScreen";
 import AddScreen from "./AddScreen";
+import {TouchableOpacity, Image } from 'react-native';
 
 const MainStacks = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,23 +19,52 @@ const Empty = () => null;
 
 const MainTabs = ({ navigation }) => {
   const [unreadCount, setUnreadCount] = useState(3);
+  const [page, setPageCount] = useState(0);
+
   return (
     <AppDrawer navigation={navigation}>
       <SafeAreaView style={{ height: "100%" }}>
+
+        
         <Tab.Navigator
           screenOptions={{
             headerShown: false,
             tabBarActiveTintColor: "#408086",
+
+            tabBarStyle: {
+              height: 90,
+              width: 350,
+              alignSelf: 'center',
+              backgroundColor: "#171717",
+              paddingLeft: 2,
+              paddingRight: 2,
+              borderRadius: 9,
+              bottom: 12,
+            },
+            tabBarItemStyle: {
+              width: 84,
+              height: 88,
+            },
+            
           }}
         >
+
+
           <Tab.Screen
             name="Home"
             component={HomeScreen}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="home" size={size} color={color} />
-              ),
-              tabBarLabel: "Home",
+              tabBarIcon: ({focused, color, size }) => {
+                let iconame = (page == 0) ? require('@/assets/HomePressed.png') : require('@/assets/Home.png');
+
+                  return (<Image style={{height: 84, width: 84}}  source={iconame}/>);
+                },
+              tabBarLabel: () => null,
+            }}
+            listeners={{
+              tabPress: () => {
+                setPageCount(0);
+              },
             }}
           />
 
@@ -42,14 +72,21 @@ const MainTabs = ({ navigation }) => {
             name="Discover"
             component={DiscoverScreen}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="search" size={size} color={color} />
-              ),
-              tabBarLabel: "Discover",
+              tabBarIcon: ({focused, color, size }) => {
+                let iconame = (page == 1) ? require('@/assets/RadarPressed.png') : require('@/assets/Radar.png');
+
+                  return (<Image style={{height: 84, width: 84}}  source={iconame}/>);
+                },
+              tabBarLabel: () => null,
+            }}
+            listeners={{
+              tabPress: () => {
+                setPageCount(1);
+              },
             }}
           />
 
-          <Tab.Screen
+          {/* <Tab.Screen
             name="AddTab"
             component={Empty} // this is a workaround to show a full screen when this tab is pressed
             options={{
@@ -64,33 +101,48 @@ const MainTabs = ({ navigation }) => {
                 navigation.navigate("Add"); // manually navigate to the stack screen outside of the tab navigators
               },
             }}
-          />
+          /> */}
 
           <Tab.Screen
             name="Inbox"
             component={NotificationsScreen}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="chatbox" size={size} color={color} />
+              tabBarIcon: ({focused, color, size }) => {
+                return (<Image style={{height: 84, width: 84}}  source={require('@/assets/Community.png')}/>);
+              },
+              tabBarLabel: () => null,
+              tabBarButton: (props) => (
+                <TouchableOpacity
+                  {...props}
+                  disabled={true}/>
               ),
-              tabBarLabel: "Inbox",
-              tabBarBadge: unreadCount,
             }}
             listeners={{
               tabPress: () => {
-                setUnreadCount(null);
+                setPageCount(2);
               },
             }}
+            
           />
 
           <Tab.Screen
             name="SettingsDrawer"
             component={MeScreen}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="person" size={size} color={color} />
+              tabBarIcon: ({focused, color, size }) => {
+                return (<Image style={{height: 84, width: 84}}  source={require('@/assets/Profile.png')}/>);
+              },
+              tabBarLabel: () => null,
+              tabBarButton: (props) => (
+                <TouchableOpacity
+                  {...props}
+                  disabled={true}/>
               ),
-              tabBarLabel: "Me",
+            }}
+            listeners={{
+              tabPress: () => {
+                setPageCount(3);
+              },
             }}
           />
         </Tab.Navigator>
